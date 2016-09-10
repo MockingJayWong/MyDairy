ab2b73ee5d87c4faf7fbd6feec2b80cb982a9380
WiQuong Wong <WiQuong Wong>
2016-09-10 21:15:40 +0800

    first -slidebar

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// (function() {
+// 	'use strict';
+	 
+// })();
+$(document).ready(function(){
+    $('.tabular.menu .item').tab();
+    $('#toggle').click(function() {
+			$('.ui.sidebar').sidebar('toggle');
+    });
+    
+
+});
